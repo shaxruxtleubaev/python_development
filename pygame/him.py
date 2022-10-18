6b718d8cc520605a29aeb24b5be952495cc4eba6
Shaxrux Tleubaev <tleubaevshaxrux@outlook.com>
2022-10-18 20:02:04 +0500

    18_10_2022 20:01

---
+++ pygame/him.py
@@ -0,0 +1,35 @@
+import pygame
+
+class He:
+    def __init__(self, game):
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
+
+        self.image = pygame.image.load('image/bd.jpg')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.moving_right = False
+        self.moving_left  = False
+        self.moving_top = False
+        self.moving_bottom = False
+    
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.bd_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.bd_speed
+        if self.moving_top and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.bd_speed
+        if self.moving_bottom and self.rect.bottom > 0:
+            self.y += self.settings.bd_speed                
+        
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blithim(self):
+        self.screen.blit(self.image, self.rect)

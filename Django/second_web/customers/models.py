f3060323a402a1c330d26086e16c615680af56b8
Shaxrux Tleubaev <tleubaevshaxrux@outlook.com>
2022-11-27 17:33:20 +0500

    27_11_2022

---
+++ Django/second_web/customers/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from datetime import date
+from django.utils.translation import gettext_lazy as _
+
+class Blog(models.Model):
+	name = models.CharField(max_length=100)
+	tagline = models.TextField()
+
+	def __str__(self):
+		return f'{self.name} - {self.tagline[:5]}...'
+
+	class Meta:
+		verbose_name_plural = _('Blog')
+
+class Author(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.EmailField()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = _('Author')
+
+class Entry(models.Model):
+	blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	headline = models.CharField(max_length=250)
+	body_text = models.TextField()
+	pub_date = models.DateField()
+	mod_date = models.DateField(default=date.today)
+	authors = models.ManyToManyField(Author)
+	number_of_comments = models.IntegerField(default=0)
+	number_of_pingbacks = models.IntegerField(default=0)
+	rating = models.IntegerField(default=5)
+
+	def __str__(self):
+		return self.headline	
+
+	class Meta:
+		verbose_name_plural = _('Entry')

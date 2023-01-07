8946c941a898a2569aa87a3ce036b2e49e46174d
Shaxrux Tleubaev <tleubaevshaxrux@outlook.com>
2023-01-07 18:07:18 +0500

    07_01_2023 18:07

---
+++ Django/TestApp/post/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ArticleListView, ArticleDetailView
+
+urlpatterns = [
+    path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
+    path('', ArticleListView.as_view(), name='article_list'),
+]

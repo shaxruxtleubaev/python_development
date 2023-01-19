17f26e63b8750a7b66c41cd06be2a5afbfacf3ca
Shaxrux Tleubaev <tleubaevshaxrux@outlook.com>
2023-01-19 13:31:14 +0500

    19_01_2023 13:31

--- Django/CRM_actual/quotes/urls.py
+++ Django/CRM_actual/quotes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from quotes.views import quote_req
 from quotes.views import QuoteList
+from quotes.views import QuoteDetail
 
 urlpatterns = [
-	path('show', QuoteList.as_view(), name='show-quotes'),
 	path('', quote_req, name='quote-req'),
-]
+	path('show/<int:pk>/', QuoteDetail.as_view(), name='quote-detail'),
+	path('show', QuoteList.as_view(), name='show-quotes'),
+]	
+
+
+##fsdsdv
--- Django/CRM_actual/src/urls.py
+++ Django/CRM_actual/src/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quote/', include('quotes.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
 
